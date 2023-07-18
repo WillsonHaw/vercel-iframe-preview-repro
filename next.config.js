cdc6f2c029fdeeb78b98364ccb352374112a5e2d
Willson Haw <willsonhaw@gmail.com>
2023-07-18 14:49:02 -0700

    Add embed page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  headers() {
+    return [
+      {
+        // Apply these headers to all routes.
+        source: "/:path*",
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
+      {
+        // Headers for embedded media
+        source: "/embed/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            // Todo: Add proper list of valid sources
+            value:
+              "frame-ancestors *.justwatch.com *.moviecycle.com http://localhost:*",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/app/embed/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>This is embedded</h1>
+      <p>You should see this if it works</p>
+    </div>
+  );
+}
